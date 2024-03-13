206c358b23a3e276219f3ed2e7af8bbf244cb7db
Vince-Williams <27vwilliams@live.ytech.edu>
2024-03-13 08:45:47 -0400

    fix?

--- jss/script.js
+++ jss/script.js
@@ -1,76 +1,33 @@
-var lightMode = getCookie('lightMode');
+var lightMode=lightMode;
 
-var lightSheet = document.getElementById("lightSheet")
-var darkSheet = document.getElementById("darkSheet")
-
-// if lightMode turned off, display dark stylesheet
-var colorSwitch = document.getElementById("colorSwitch")
-colorSwitch.innerText = ""
-
-if (lightMode == 'false') {
-    console.log("Turning dark mode on...");
-    colorSwitch.innerText = "Light Mode"
-    darkSheet.media = ""
-
-}
-else {
-    {
-        console.log("Turning light mode on...");
-        colorSwitch.innerText = "Dark Mode"
-        darkSheet.media = "none"
-
-
-    }
-}
-
-function changeSheet() {
-    //make lightMode equal NOT lightMode
+function changeSheet(){
+//make lightMode equal NOT lightMode
     lightMode = !lightMode;
-    setCookie("lightMode", lightMode, 1)
 
+    var lightSheet = document.getElementById("lightSheet")
+    var darkSheet = document.getElementById("darkSheet")
 
-    colorSwitch.innerText = ""
+    // if lightMode turned off, display dark stylesheet
+    var colorSwitch = document.getElementById("colorSwitch")
 
-    if (lightMode == 'false') {
+    colorSwitch.innerText= ""
+
+    if(lightMode == false){
         console.log("Turning dark mode on...");
-        colorSwitch.innerText = "Light Mode"
+        colorSwitch.innerText= "Light Mode"
         darkSheet.media = ""
-
+        lightSheet.media = "none"
     }
     else {
         {
-            console.log("Turning light mode on...");
-            colorSwitch.innerText = "Dark Mode"
-            darkSheet.media = "none"
-
-        }
+        console.log("Turning light mode on...");
+        colorSwitch.innerText= "Dark Mode"
+        darkSheet.media = "none"
+        lightSheet.media = ""
+    }
     }
 
 
 
-}
-
-
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
 }
 
